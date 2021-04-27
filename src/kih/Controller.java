c931136062f9a274481c1b1fdec92a7e384e8f2f
VitalikZX <iit622m@mail.ru>
2021-04-27 21:53:12 +0500

    Do view. But not do logic.

--- src/kih/Controller.java
+++ src/kih/Controller.java
@@ -1,11 +1,17 @@
 package kih;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 public class Controller {
+    private String [] sRadix = {"dec", "hex"};
+    private ObservableList<String> radix = FXCollections.observableArrayList(sRadix);
+
     @FXML
     private Button bttnCount;
 
@@ -24,9 +30,39 @@ public class Controller {
     private TextField out_data_file_name;
 
     @FXML
+    private ChoiceBox in_data_radix;
+    @FXML
+    private ChoiceBox h_file_radix;
+    @FXML
+    private ChoiceBox out_data_radix;
+
+    @FXML
     private LineChart<Number,Number> data;
     @FXML
     private LineChart<Number,Number> core;
     @FXML
     private LineChart<Number,Number> afh;
+
+    @FXML //запускается только при старте
+    private void initialize() {
+        in_data_radix.setItems(radix);
+        in_data_radix.setValue(sRadix[0]);
+
+        h_file_radix.setItems(radix);
+        h_file_radix.setValue(sRadix[0]);
+
+        out_data_radix.setItems(radix);
+        out_data_radix.setValue(sRadix[0]);
+
+    }
+
+    @FXML
+    private void count(){
+        System.out.println("count");
+    }
+
+    @FXML
+    private void save(){
+        System.out.println("save");
+    }
 }
